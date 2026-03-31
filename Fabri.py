08ce14e90862f65b06b596b25421a9256a76afc6
“Fabri-Programmer” <fabrizziopks@hotmail.com>
2026-03-30 19:04:45 -0500

    first commit

---
+++ Fabri.py
@@ -0,0 +1,7 @@
+print("Buenas tardes")
+edad = 18
+nomb = "fabri"
+nob = 'llara'
+altura = 1.8
+esa = true
+print(f"tu nombre es {nomb} {nob} y tu edad es {edad} y tu carro ("esa if  " es verde" else esa)
