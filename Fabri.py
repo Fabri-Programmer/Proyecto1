9160fdf3948d22c8c41c3b2516c24968032276ed
“Fabri-Programmer” <fabrizziopks@hotmail.com>
2026-03-30 19:15:46 -0500

    cambio

--- Fabri.py
+++ Fabri.py
@@ -3,5 +3,5 @@ edad = 18
 nomb = "fabri"
 nob = 'llara'
 altura = 1.8
-esa = true
-print(f"tu nombre es {nomb} {nob} y tu edad es {edad} y tu carro ("esa if  " es verde" else esa)
+esa = True
+print(f"tu nombre es {nomb} {nob} y tu edad es {edad} y tu carro))
